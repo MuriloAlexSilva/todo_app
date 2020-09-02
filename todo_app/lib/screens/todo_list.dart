a53cce4eff54d0b5edf6f1b8cef13181b434b9da
Murilo Silva <murilo.alex.silva@gmail.com>
2020-09-02 10:34:27 -0300

    app

---
+++ todo_app/lib/screens/todo_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Todo extends StatefulWidget {
+  @override
+  _TodoState createState() => _TodoState();
+}
+
+class _TodoState extends State<Todo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
